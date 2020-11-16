d1bb3050efb15a4225a056ac251fdff8854dd46b
Maksym Honchar <maksim.honchar@gmail.com>
2020-11-17 00:34:31 +0200

    project prepared

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
       <p>React</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
